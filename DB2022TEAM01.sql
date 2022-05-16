0a32cf94fdd3432a2b6aaf8fa6cbf8560c740725
SEON UI LEE <sunnyinsummer@ewhain.net>
2022-05-16 15:38:01 +0900

    [선의] db mysql script 수정

--- DB2022TEAM01.sql
+++ DB2022TEAM01.sql
@@ -2,53 +2,61 @@
 create database DB2022Team01;
 use DB2022Team01;
 
+
+
 # db 유저 생성 + 권한 부여
 create user 'DB2022Team01'@localhost identified by 'DB2022Team01';
 grant all privileges on DB2022Team01.* to 'DB2022Team01'@localhost;
 
 # 테이블 생성
 create table `DB2022_user`(
-	`id` BIGINT primary key,
+	`id` BIGINT not null auto_increment primary key,
     `name` varchar(45) unique key,
     `password` varchar(45)
 );
 
+# 두개 묶어서 primary key -> ?
+# group 변수명때문에 오류 났던 것 같음
 create table `DB2022_idol`(
-	`group` varchar(45) primary key,
-    `member` varchar(45) primary key,
+	`id` bigint not null auto_increment primary key,
+	`gp` varchar(45) not null,
+    `member` varchar(45) not null,
     `quantity` BIGINT
 );
 
+# 아이돌에 아이디 주고,,, 저거 굳이...?
 create table `DB2022_product`(
-	`id` BIGINT primary key,
+	`id` BIGINT not null auto_increment primary key,
     `name` varchar(45),
     `price` BIGINT,
     `seller` varchar(45),
     `category` varchar(45),
+    `idol_id` bigint,
+    `idol_group` varchar(45),
+    `idol_member` varchar(45),
     `isSold` bool,
     `date` date,
-    foreign key(seller) references user(name)
+    foreign key(idol_id) references DB2022_idol(id),
+    foreign key(seller) references DB2022_user(name)
 );
 
+
+
 create table `DB2022_trade` (
-	`id` bigint primary key,
+	`id` bigint auto_increment primary key,
     `product_id` bigint,
     `name` varchar(45),
     `price` bigint,
     `seller` varchar(45),
     `buyer` varchar(45),
-	foreign key(product_id) references product(id),
-    foreign key(seller) references user(name),
-    foreign key(buyer) references user(name)
+	foreign key(product_id) references DB2022_product(id),
+    foreign key(seller) references DB2022_user(name),
+    foreign key(buyer) references DB2022_user(name)
 );
 
 create table `DB2022_wishlist`(
-	`id` bigint primary key,
-    `user_name` varchar(45),
+    `user_id` varchar(45),
     `product_id` bigint,
-    `product_name` varchar(45),
-    `product_category` varchar(45),
-	`product_price` bigint,
-    foreign key(user_name) references user(name),
-    foreign key(product_id) references product(id)
+    foreign key(user_id) references DB2022_user(id),
+    foreign key(product_id) references DB2022_product(id)
 );
