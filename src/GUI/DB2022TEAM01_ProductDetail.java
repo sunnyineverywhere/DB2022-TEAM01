6144184de5f40dba6eb7ebcd5ddd77ed23c7d70e
seojeewon <jw07061@ewhain.net>
2022-05-31 23:24:07 +0900

    [지원] 상품 상세 수정

--- src/GUI/DB2022TEAM01_ProductDetail.java
+++ src/GUI/DB2022TEAM01_ProductDetail.java
@@ -40,19 +40,23 @@ public class DB2022TEAM01_ProductDetail {
 
 
 	public DB2022TEAM01_ProductDetail() {
-		JFrame frame = new JFrame("상품 상세"); 
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("상품 상세");
-        JButton bt1 = new JButton("찜");
-        JButton bt2 = new JButton("매수");
+		JFrame frame = new JFrame("상품 상세");
+		Container contentPane = frame.getContentPane();
+		
+		contentPane.setBackground(Color.white);
+		contentPane.setLayout(null);
+        JLabel label = new JLabel("상품 상세");        
         
-        Font font = new Font("맑은 고딕", Font.BOLD, 20);
+        Font font = new Font("맑은 고딕", Font.BOLD, 20);             
         
+        //페이지 제목
+        label.setBounds(453, 23, 94, 25);
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setFont(font);
+        contentPane.add(label);
  
         
-        String col[] = { "상품명", "아이돌 그룹", "멤버명", "카테고리" , "매도자 ID", "가격", "등록 날짜", "찜", "매수" };   
+        String col[] = { "상품 ID", "상품명", "아이돌 그룹", "멤버명", "카테고리" , "매도자 ID", "가격", "등록 날짜" };   
 
 		DefaultTableModel model = new DefaultTableModel(col, 0);
 
@@ -66,6 +70,8 @@ public class DB2022TEAM01_ProductDetail {
 			while(rs.next()){
 				Vector record = new Vector();
 				row++;
+				//상품 id
+				record.add(rs.getInt("id"));
 				 // 샹품명
 				record.add(rs.getString("name"));
 
@@ -92,25 +98,65 @@ public class DB2022TEAM01_ProductDetail {
 			e.printStackTrace();
 		}
 
-
-
         JTable table = new JTable(model);
         table.setRowHeight(30);
-        table.getColumnModel().getColumn(7).setCellRenderer(new TableCell1());
-        table.getColumnModel().getColumn(7).setCellEditor(new TableCell1());;
-
         
-        table.getColumnModel().getColumn(8).setCellRenderer(new TableCell2());
-        table.getColumnModel().getColumn(8).setCellEditor(new TableCell2());;
-        
-        table.setPreferredScrollableViewportSize(new Dimension (950, 650));
+        table.setPreferredScrollableViewportSize(new Dimension (900, 650));
         table.setBackground(Color.pink); 
         
         JButton home = DB2022TEAM01_Main.make_home();
         home.setBounds(950, 5, 30, 30);
-        panel.add(home);
+        contentPane.add(home);
 
-
+        //찜, 매수하기
+        JLabel idInput_label = new JLabel("상품 ID:");
+        JTextField idInput = new JTextField(10);
+        JButton bt1 = new JButton("찜");
+        JButton bt2 = new JButton("매수");
+        
+        Font font2 = new Font("맑은 고딕", Font.BOLD, 15);
+        Font font3 = new Font("맑은 고딕", Font.BOLD, 11);
+        Color btn_color=new Color(0xFF6472);
+		
+        idInput_label.setFont(font2);
+        bt1.setFont(font3);
+        bt1.setBackground(btn_color);
+		bt1.setForeground(Color.white);
+        bt2.setFont(font3);
+        bt2.setBackground(btn_color);
+		bt2.setForeground(Color.white);
+        
+        idInput_label.setBounds(298, 600, 58, 20);
+        idInput.setBounds(369, 600, 178, 26);
+        bt1.setBounds(567, 600, 45, 26);
+        bt2.setBounds(621, 600, 60, 26);
+        
+        //찜 버튼 눌렀을 때
+        bt1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new PopUp1();
+				//여기에 위시리스트에 추가하는 코드
+			}
+		});
+        
+        //매수 버튼 눌렀을 때
+        bt2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new PopUp2();
+				//여기에 구매 처리하는 코드
+			}
+		});
+        
+        contentPane.add(idInput_label);
+        contentPane.add(idInput);
+        contentPane.add(bt1);
+        contentPane.add(bt2);
         
         //홈버튼
         home.addActionListener(new ActionListener() {
@@ -124,17 +170,16 @@ public class DB2022TEAM01_ProductDetail {
 		});
       
         JScrollPane pane = new JScrollPane(table); 
-        panel.setLayout(new BorderLayout(10, 10));
-        panel.add(pane, BorderLayout.CENTER);
-        panel.add(label, BorderLayout.NORTH);
-        frame.add(panel); 
+        pane.setBounds(0, 60, 988, 510);
+        contentPane.add(pane);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setSize(1000, 700);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);	//화면 중앙에 뜸
         frame.setVisible(true); 
-	}	
+	}
+	
 }
 
 class PopUp1 extends JFrame{
@@ -179,69 +224,3 @@ class PopUp2 extends JFrame{
 	}
 }
 
-//btn1(찜 버튼 셀에 추가)
-class TableCell1 extends AbstractCellEditor implements TableCellEditor, TableCellRenderer {
-
-	JButton btn1;
-	
-	public TableCell1() {
-		btn1 = new JButton("찜");
-		
-		btn1.addActionListener(e -> {
-			new PopUp1();
-		});
-	}
-	
-	@Override
-	public Object getCellEditorValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		// TODO Auto-generated method stub
-		return btn1;
-	}
-
-	@Override
-	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-		// TODO Auto-generated method stub
-		return btn1;
-	}
-	
-}
-//btn2(매수 버튼 셀에 추가)
-class TableCell2 extends AbstractCellEditor implements TableCellEditor, TableCellRenderer {
-
-	JButton btn2;
-	
-	public TableCell2() {
-		btn2 = new JButton("매수");
-		btn2.addActionListener(e -> {
-
-			new PopUp2();
-		});
-	}
-	
-	@Override
-	public Object getCellEditorValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		// TODO Auto-generated method stub
-		return btn2;
-	}
-
-	@Override
-	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-		// TODO Auto-generated method stub
-		return btn2;
-	}
-	
-}
