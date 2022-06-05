d92515dcdbf29a59a7904848a1c63496964d1f5e
ayla kim <75625296+ayla-12@users.noreply.github.com>
2022-06-05 16:41:01 +0900

    SQL 파일
    요구사항에 create, dropdb 분리하라고 적혀있어 sql 다시 작성해올립니다.

---
+++ sql/create.sql
@@ -0,0 +1,74 @@
+# db 생성
+create database DB2022Team01;
+use DB2022Team01;
+
+# db 유저 생성 + 권한 부여
+# create user 'DB2022Team01'@localhost identified by 'DB2022Team01';
+grant all privileges on DB2022Team01.* to 'DB2022Team01'@localhost;
+
+# 테이블 생성
+# 유저 테이블
+create table `DB2022_user`(
+	`id` BIGINT not null auto_increment primary key,
+    `name` varchar(45) unique key,
+    `password` varchar(45),
+    `login` bool default false
+);
+
+# 아이돌 테이블
+create table `DB2022_idol`(
+	`idol_id` bigint not null auto_increment,
+	`gp` varchar(45) not null,
+    `member` varchar(45) not null,
+    primary key(idol_id)
+);
+
+# 상품 테이블
+create table `DB2022_product`(
+	`id` BIGINT not null auto_increment primary key,
+    `name` varchar(45),
+    `user_id` BIGINT,
+    `price` BIGINT,
+    `seller` varchar(45),
+    `category` varchar(255),
+    `idol_id` bigint,
+    `isSold` bool default false,
+    `date` date,
+    foreign key(user_id) references DB2022_user(id),
+    foreign key(idol_id) references DB2022_idol(idol_id),
+    foreign key(seller) references DB2022_user(name),
+    check((category) in ('포토카드', '앨범', '인형', '시즌그리팅', '공식키트', '폴라로이드', '포스터', '잡지', '기타'))
+);
+
+# 유저의 거래 내역 테이블
+create table `DB2022_trade` (
+	`trade_id` bigint auto_increment primary key,
+    `product_id` bigint,
+    `buyer_id` bigint default null,
+    `buyer_name` varchar(45) default null,
+	foreign key(product_id) references DB2022_product(id),
+    foreign key(buyer_id) references DB2022_user(id),
+    foreign key(buyer_name) references DB2022_user(name)
+);
+
+# 위시리스트 테이블
+create table `DB2022_wishlist`(
+    `user_id` bigint,
+    `product_id` bigint,
+    foreign key(user_id) references DB2022_user(id),
+    foreign key(product_id) references DB2022_product(id)
+);
+
+
+# 인덱스 생성
+create index idx_idol on db2022team01.db2022_idol
+(gp, member);
+
+create index idx_user on db2022team01.db2022_user
+(name, password);
+
+create index idx_category on db2022team01.db2022_product
+(category);
+
+create index idx_user_login on db2022team01.db2022_user
+(login);
---
+++ sql/dropdb.sql
@@ -0,0 +1,12 @@
+#DROP
+use db2022team01;
+
+# 스키마의 순서에 맞춰 drop query 실행
+DROP TABLE db2022_wishlist;
+DROP TABLE db2022_trade;
+DROP TABLE db2022_product;
+DROP TABLE db2022_user;
+DROP TABLE db2022_idol;
+
+# 데이터베이스도 drop
+DROP DATABASE db2022team01;
